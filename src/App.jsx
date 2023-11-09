cd643e9ee193895fffd878a0b4d642d078209931
Zajaczkowski23 <krystian147@spoko.pl>
2023-11-10 00:54:16 +0100

    Added data to work with from web scraper

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,10 @@ import Nav from "./components/Nav/Nav";
 import useFetch from "./hooks/fetchDataHook";
 
 function App() {
-  const options = {
-    method: "GET",
-    url: "https://livescore6.p.rapidapi.com/matches/v2/list-live",
-    params: {
-      Category: "soccer",
-      Timezone: "-7",
-    },
-    headers: {
-      "X-RapidAPI-Key": "c1fd44d168msh200f557f08e7ddfp1259f6jsn9c37a26fb935",
-      "X-RapidAPI-Host": "livescore6.p.rapidapi.com",
-    },
-  };
+  const api = "http://localhost:3000/events";
+  const { data, loading, error } = useFetch(api);
 
-  // const { data, loading, error } = useFetch(options);
+  console.log(data);
 
   return (
     <main className="main">
