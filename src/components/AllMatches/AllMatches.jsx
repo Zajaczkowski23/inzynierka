d4836b944114697e0b280b9e6762907584c9b49b
Zajaczkowski23 <krystian147@spoko.pl>
2023-11-16 22:58:43 +0100

    Grouped matches based on where they play (league or tournament)

--- src/components/AllMatches/AllMatches.jsx
+++ src/components/AllMatches/AllMatches.jsx
@@ -5,55 +5,68 @@ function AllMatches() {
   const api = "http://localhost:3000/events";
   const { data } = useFetch(api);
 
+  // Group matches by both tournament name and category name
+  const groupedMatches = {};
+  if (data) {
+    data.forEach((matchInfo) => {
+      const tournamentName = matchInfo.tournament.name;
+      const categoryName = matchInfo.tournament.category.name;
+      const key = `${tournamentName}_${categoryName}`;
+
+      if (!groupedMatches[key]) {
+        groupedMatches[key] = [];
+      }
+      groupedMatches[key].push(matchInfo);
+    });
+  }
+
   return (
     <div className="data-section">
-      {data &&
-        data.map((matchInfo) => {
-          // Convert timestamp to a human-readable time
-          const convertTime = new Date(matchInfo.startTimestamp * 1000);
-          const hours = convertTime.getUTCHours();
-          let minutes = convertTime.getUTCMinutes();
+      {Object.keys(groupedMatches).map((key) => {
+        const [tournamentName, categoryName] = key.split("_");
 
-          {
-            /* const convertCurrTime = new Date(
-            matchInfo.statusTime.timestamp * 1000
-          );
-          const hoursCurr = convertCurrTime.getUTCHours();
-          let minutesCurr = convertCurrTime.getUTCMinutes(); */
-          }
+        return (
+          <div key={key}>
+            <h3>{categoryName}</h3>
+            <h2>{tournamentName}</h2>
 
-          if (minutes === 0) {
-            minutes = "00";
-          }
+            {groupedMatches[key].map((matchInfo) => {
+              // Convert timestamp to a human-readable time
+              const convertTime = new Date(matchInfo.startTimestamp * 1000);
+              const hours = convertTime.getUTCHours();
+              let minutes = convertTime.getUTCMinutes();
 
-          return (
-            <div className="data-section__group" key={matchInfo.customId}>
-              <div className="data-section__country">
-                <div className="data-section__tournament-info">
+              if (minutes === 0) {
+                minutes = "00";
+              }
+
+              return (
+                <div className="data-section__group" key={matchInfo.customId}>
                   <div className="data-section__country">
-                    {matchInfo.tournament.category.name}
-                  </div>
-                  <div className="data-section__tournament-name">
-                    {matchInfo.tournament.name}
+                    <div className="data-section__match-info">
+                      <div className="data-section__start-match">
+                        <div className="data-section__start-time">{`${hours}:${minutes}`}</div>
+                      </div>
+                      <div className="data-section__teams">
+                        <div className="team">{matchInfo.homeTeam.name}</div>
+                        <div className="team">{matchInfo.awayTeam.name}</div>
+                      </div>
+                      <div className="data-section__score">
+                        <div className="score">
+                          {matchInfo.homeScore.current}
+                        </div>
+                        <div className="score">
+                          {matchInfo.awayScore.current}
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
-                <div className="data-section__match-info">
-                  <div className="data-section__start-match">
-                    <div className="data-section__start-time">{`${hours}:${minutes}`}</div>
-                  </div>
-                  <div className="data-section__teams">
-                    <div className="team">{matchInfo.homeTeam.name}</div>
-                    <div className="team">{matchInfo.awayTeam.name}</div>
-                  </div>
-                  <div className="data-section__score">
-                    <div className="score">{matchInfo.homeScore.current}</div>
-                    <div className="score">{matchInfo.awayScore.current}</div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          );
-        })}
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
